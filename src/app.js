65b758bcc631933ad3b431da2addfaca49797abf
nikhilknick <nikhilknick@gmail.com>
2018-09-19 00:09:07 +0530

    fetching expenses

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import {startSetExpenses} from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import './firebase/firebase';
+
 
 const store = configureStore();
 
@@ -19,4 +20,10 @@ const jsx = (
   </Provider>
 );
 
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(()=>{
 ReactDOM.render(jsx, document.getElementById('app'));
+});
+
+
