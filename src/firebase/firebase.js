567ff4af1ba0c8b062c4fced6ad659a249e12ddb
nikhilknick <nikhilknick@gmail.com>
2018-09-20 11:04:10 +0530

    add private routes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -12,7 +12,11 @@ const config = {
 
   const database = firebase.database();
 
-  export {firebase,database as default}; 
+  const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+
+
+  export {firebase,googleAuthProvider,database as default}; 
 
   // database.ref('expenses')
   // .once('value')
