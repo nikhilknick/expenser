48cf81f1f7311528b26d0b019913cc611b21e973
nikhilknick <nikhilknick@gmail.com>
2018-09-21 18:37:36 +0530

    add styles

---
+++ src/tests/components/LoadingPage.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import LoadingPage from '../../components/LoadingPage';
+
+
+test('should render Loading Page correctly', () => {
+    const wrapper = shallow(<LoadingPage />);
+    expect(wrapper).toMatchSnapshot();
+  });
